360961d21b5182d241f5cec3a84ffad89b90b2bd
David Christian <davidchristian@gmail.com>
2020-05-27 22:20:00 -0700

    Added basic drawer

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -1,6 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
+import Drawer from '@material-ui/core/Drawer';
 import Typography from '@material-ui/core/Typography';
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const useStyles = makeStyles(() => ({
 	menuContainer: {
@@ -37,11 +41,26 @@ const useStyles = makeStyles(() => ({
 
 const Menu = () => {
 	const classes = useStyles();
+	const [state, setState] = useState(false);
+
+	const toggleDrawer = () => event => {
+		if (
+			event.type === 'keydown' &&
+			(event.key === 'Tab' || event.key === 'Shift')
+		) {
+			return;
+		}
+		console.log('---state', state);
+		setState(!state);
+	};
 
 	return (
 		<>
 			<div className={classes.mobileMenuContainer}>
-				<MenuRoundedIcon className={classes.mobileMenu} />
+				<MenuRoundedIcon
+					onClick={toggleDrawer()}
+					className={classes.mobileMenu}
+				/>
 			</div>
 			<div className={classes.menuContainer}>
 				<Typography className={classes.menuItem}>About</Typography>
@@ -51,6 +70,16 @@ const Menu = () => {
 				</Typography>
 				<Typography className={classes.menuItem}>Contact</Typography>
 			</div>
+			<Drawer
+				anchor="left"
+				variant="temporary"
+				open={state}
+				onClose={toggleDrawer()}
+			>
+				<ListItem>
+					<ListItemText primary="text" />
+				</ListItem>
+			</Drawer>
 		</>
 	);
 };
