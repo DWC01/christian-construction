76891616e00f8cea3d4c08c4871f7b29a287610f
David Christian <davidchristian@gmail.com>
2020-05-27 23:37:36 -0700

    Added menu drawer

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -1,12 +1,33 @@
 import { makeStyles } from '@material-ui/core/styles';
 import { useState } from 'react';
+import List from '@material-ui/core/List';
 import Drawer from '@material-ui/core/Drawer';
+import ListItem from '@material-ui/core/ListItem';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
-import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
+	list: {
+		width: '100vw',
+	},
+	listItem: {
+		textAlign: 'center',
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: theme.spacing(0, 1),
+		...theme.mixins.toolbar,
+	},
+	BackdropProps: {
+		backgroundColor: 'rgba(250, 250, 250, 0.5)',
+	},
+	paper: {
+		boxShadow: 'unset',
+	},
 	menuContainer: {
 		display: 'flex',
 		alignItems: 'center',
@@ -35,6 +56,7 @@ const useStyles = makeStyles(() => ({
 		},
 		mobileMenu: {
 			fontSize: '2rem',
+			color: theme.palette.action.active,
 		},
 	},
 }));
@@ -50,17 +72,19 @@ const Menu = () => {
 		) {
 			return;
 		}
-		console.log('---state', state);
+
 		setState(!state);
 	};
 
 	return (
 		<>
 			<div className={classes.mobileMenuContainer}>
-				<MenuRoundedIcon
-					onClick={toggleDrawer()}
-					className={classes.mobileMenu}
-				/>
+				<IconButton onClick={toggleDrawer()}>
+					<MenuRoundedIcon
+						onClick={toggleDrawer()}
+						className={classes.mobileMenu}
+					/>
+				</IconButton>
 			</div>
 			<div className={classes.menuContainer}>
 				<Typography className={classes.menuItem}>About</Typography>
@@ -71,14 +95,42 @@ const Menu = () => {
 				<Typography className={classes.menuItem}>Contact</Typography>
 			</div>
 			<Drawer
+				classes={{
+					paper: classes.paper,
+				}}
+				ModalProps={{
+					Paper: {
+						root: classes.Paper,
+					},
+					BackdropProps: {
+						classes: {
+							root: classes.BackdropProps,
+						},
+					},
+				}}
 				anchor="left"
 				variant="temporary"
 				open={state}
 				onClose={toggleDrawer()}
 			>
-				<ListItem>
-					<ListItemText primary="text" />
-				</ListItem>
+				<div className={classes.drawerHeader}>
+					<IconButton onClick={toggleDrawer()}>
+						<CloseRoundedIcon />
+					</IconButton>
+				</div>
+				<List className={classes.list}>
+					{['About', 'Portfolio', 'Our Process', 'Get Started'].map(
+						text => (
+							<ListItem
+								key={text}
+								className={classes.listItem}
+								button
+							>
+								<ListItemText primary={text} />
+							</ListItem>
+						),
+					)}
+				</List>
 			</Drawer>
 		</>
 	);
