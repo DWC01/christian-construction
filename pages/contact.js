a2e4de10af8a14607efc82309aff6dcc9f3131d3
David Christian <davidchristian@gmail.com>
2020-10-12 17:11:47 -0700

    Added contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,20 @@
-const contact = () => {
-	return <>Coming Soon..</>;
+import { makeStyles } from '@material-ui/core/styles';
+import Contact from '../src/components/home/Contact';
+
+const useStyles = makeStyles(() => ({
+	contactContainer: {
+		margin: '50px 0 0 0',
+	},
+}));
+
+const ContactPage = () => {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.contactContainer}>
+			<Contact />
+		</div>
+	);
 };
 
-export default contact;
+export default ContactPage;
